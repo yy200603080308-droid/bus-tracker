ce379f012e15091837c0c17cd69955bd71b3281e
yy200603080308-droid <y.y200603080308@gmail.com>
2026-03-24 21:41:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import requests
 import re
 import time
 
-URL = "https://kyotocity.bus-navigation.jp/wgsys/wgs_kyt/vehiclePosition.htm?tabName=vehiclePosition&selectedLandmarkCatCd=&from=%E6%96%B0%E6%9E%97%E5%85%AC%E5%9B%A3%E4%BD%8F%E5%AE%85%E5%89%8D&fromType=1&to=&toType=&locale=ja&fromlat=34.966261&fromlng=135.670186&tolat=&tolng=&fromSignpoleKey=&routeLayoutCd=&bsid=1&fromBusStopCd=&toBusStopCd=&mapFlag=true&existYn=&routeKey=&nextDiagramFlag=&diaRevisedDate=&timeTableDirevtionCd=&searchDate=&searchTime=&fromBusStopKey=&toBusStopKey=&lineSelected=&targetTabName=mapSearchTab&informationTextId=&busStopName=%E6%96%B0%E6%9E%97%E5%85%AC%E5%9B%A3%E4%BD%8F%E5%AE%85%E5%89%8D&search_map_from=&search_map_to=&routeKeys=9834_9849_9856%2C9857_10094%2C10119_10098%2C10097_10130_9833_9851_9859%2C9858_10095&fromDisplayPassNo=28_6_5%2C5_6%2C16_7%2C7_6_2_32_27%2C28_11&toDisplayPassNo=&routeKeynum=10&busCode=&formerApproachLat=&formerApproachLng=&formerApproachGuidance=3637%3A5%3A1%402973%3A3%3A1%401746%3A4%3A1&informationFromTextId=&informationToTextId=&informationRouteId=&busstopCdnum=&autoRefreshTime=&vehicleScrollPosition=&destinationCd=023020003%2C029010001%2C033033T01%2CW01W05001%2CW02010002%2CW08020002%2C023010001%2C029020003%2C033033T06%2CW01020001&fromSignpoleStringKey=29690_29690_29690%2C29690_29690%2C29690_29690%2C29690_29690_29691_29691_29691%2C29691_29691&routeSelectNo=0"
-
-TARGET_BUS = "2973"
+TARGET_BUS = "2651"
 WEBHOOK_URL = "https://discord.com/api/webhooks/1485962823250215004/2llIF6XkFAKSvOVZ23ZNABTAqccSXb966k2HyvRJkBI67tDXewx2Jglw-bngzhT7qBuq"
 
+URL = "https://kyotocity.bus-navigation.jp/wgsys/wgs_kyt/search.htm"
+
 last_seen = None
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://kyotocity.bus-navigation.jp/"
+}
 
 def send(msg):
     requests.post(WEBHOOK_URL, json={"content": msg})
@@ -15,27 +20,21 @@ def send(msg):
 
 def get_buses():
     try:
-        res = requests.get(URL, timeout=5)
+        res = requests.get(URL, headers=headers, timeout=5)
         text = res.text
 
-        # ★ここがポイント
-        match = re.search(r'formerApproachGuidance=([^"&]+)', text)
+        # ★ここが核心
+        match = re.findall(r'(\d{4}:\d+:\d+)', text)
 
-        if match:
-            raw = match.group(1)
-            buses = raw.split("@")
-            return buses
-
-        return []
+        print("取得:", match)
+        return match
 
     except Exception as e:
-        print("取得エラー:", e)
+        print("エラー:", e)
         return []
 
 while True:
     buses = get_buses()
-
-    print("取得:", buses)
 
     for bus in buses:
         if TARGET_BUS in bus:
